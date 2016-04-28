8ff98516198dcfc50fe2f60c86db1e135e80f4ba
joshmutus <robson.massif@gmail.com>
2016-04-28 16:41:21 -0700

    Simplify API example to remove extraneous lists, etc.

--- gallery_api.py
+++ gallery_api.py
@@ -11,26 +11,17 @@ class Greeting(messages.Message):
   """Greeting that stores a message."""
   message = messages.StringField(1)
 
-
-class GreetingCollection(messages.Message):
-  """Collection of Greetings."""
-  items = messages.MessageField(Greeting, 1, repeated=True)
-
-
-STORED_GREETINGS = GreetingCollection(items=[
-    Greeting(message='Hello world!'),
-    Greeting(message='goodbye world!'),
-])
+hello_grt = Greeting(message="Hi World")
 
 @endpoints.api(name='gallery', version='v1')
 class GalleryApi(remote.Service):
   """Helloworld API v1."""
 
-  @endpoints.method(message_types.VoidMessage, GreetingCollection,
+  @endpoints.method(message_types.VoidMessage, Greeting,
                     path='gallery', http_method='GET',
-                    name='greetings.listGreeting')
+                    name='messages.sayHello')
   def greetings_list(self, unused_request):
-    return STORED_GREETINGS
+    return hello_grt
 
   ID_RESOURCE = endpoints.ResourceContainer(
       message_types.VoidMessage,
