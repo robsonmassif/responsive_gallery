34f48fcdf820a38934082e3769ad3e68c9d4a832
joshmutus <robson.massif@gmail.com>
2016-04-29 21:42:30 -0700

    Gallery Labels loaded via endpoints

--- gallery_api.py
+++ gallery_api.py
@@ -11,7 +11,17 @@ class Greeting(messages.Message):
   """Greeting that stores a message."""
   message = messages.StringField(1)
 
+class LabelCollection(messages.Message):
+  """Collection of Labels."""
+  items = messages.MessageField(Greeting, 1, repeated=True)
+
 hello_grt = Greeting(message="Hi World")
+
+local_labels = ["A. Art", "B. Free", "C. Saw", "D. Lite"]
+
+STORED_LABELS = LabelCollection(items=[Greeting(message=label) for label in local_labels])
+
+next_label = Greeting(message="A. Art")
 
 @endpoints.api(name='gallery', version='v1')
 class GalleryApi(remote.Service):
@@ -22,6 +32,15 @@ class GalleryApi(remote.Service):
                     name='messages.sayHello')
   def greetings_list(self, unused_request):
     return hello_grt
+
+
+  @endpoints.method(message_types.VoidMessage, LabelCollection,
+                    path='labels', http_method='GET',
+                    name='fill.labels')
+  def label_list(self, unused_request):
+    return STORED_LABELS
+
+
 
   ID_RESOURCE = endpoints.ResourceContainer(
       message_types.VoidMessage,
